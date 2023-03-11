818747fd3bd6df7d6c7f0734c8483092f8a072e4
aykutozturk <aykut.ozturk@hotmail.com>
2023-03-11 16:18:57 +0300

    userforlogindto added firstname and lastname

--- OnlineLibrary.Entities/Dtos/UserForLoginDto.cs
+++ OnlineLibrary.Entities/Dtos/UserForLoginDto.cs
@@ -4,6 +4,8 @@ namespace OnlineLibrary.Entities.Dtos
 {
     public class UserForLoginDto : IDto
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
     }
